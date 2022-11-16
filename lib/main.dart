0c8ecc4f46ab7bf57e587622aab81b726bfc7384
Teuku Zafran Elrifqi <57694121+zafran72@users.noreply.github.com>
2022-11-17 00:53:45 +0700

    chore: config firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:barberia/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
 import 'injection.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   init();
   runApp(const MyApp());
 }
