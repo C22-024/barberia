81da752ae7fcab7d0fe66b829029884cc0e75eb9
Michael Elian Kevin <michaelkevin47@gmail.com>
2022-11-25 17:53:05 +0700

    chore(auth)!: implement welcome page
    Impelement routing.
    Implement injection.
    Add spacing widgets.
    Update `PlaceholderPage` with an `BAppBar`.
    
    BREAKING CHANGE: update minSdk to 26.

---
+++ lib/common_widgets/spacing.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+const verticalSpace4 = SizedBox(height: 4);
+const verticalSpace8 = SizedBox(height: 8);
+const verticalSpace16 = SizedBox(height: 16);
+const verticalSpace32 = SizedBox(height: 32);
+
+const horizontalSpace4 = SizedBox(width: 4);
+const horizontalSpace8 = SizedBox(width: 8);
+const horizontalSpace16 = SizedBox(width: 16);
+const horizontalSpace32 = SizedBox(width: 32);
---
+++ lib/features/auth/presentation/pages/welcome_page.dart
@@ -0,0 +1,119 @@
+import 'package:barberia_ui/barberia_ui.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../../common_widgets/spacing.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        alignment: AlignmentDirectional.bottomCenter,
+        children: const [
+          _Background(),
+          _Foreground(),
+        ],
+      ),
+    );
+  }
+}
+
+class _Background extends StatelessWidget {
+  const _Background();
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.bottomCenter,
+          end: Alignment.topCenter,
+          colors: [
+            BColors.neutral1000,
+            BColors.transparent,
+          ],
+        ),
+      ),
+      position: DecorationPosition.foreground,
+      child: Image.asset(
+        'assets/images/welcome_background.png',
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
+
+class _Foreground extends StatelessWidget {
+  const _Foreground();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: const [
+          _DisplayTextSection(),
+          _ButtonSection(),
+        ],
+      ),
+    );
+  }
+}
+
+class _DisplayTextSection extends StatelessWidget {
+  const _DisplayTextSection();
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Center(
+        child: Hero(
+          tag: 'display-text',
+          child: Material(
+            type: MaterialType.transparency,
+            child: BText.display(
+              'Temukan Barbershop terbaik di sekitarmu',
+              maxLines: 4,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _ButtonSection extends StatelessWidget {
+  const _ButtonSection();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Hero(
+          tag: 'button0',
+          child: BButton(
+            label: 'Buat Akun Baru',
+            onPressed: () {
+              context.pushNamed('signup');
+            },
+          ),
+        ),
+        verticalSpace8,
+        Hero(
+          tag: 'button1',
+          child: BButton(
+            label: 'Masuk',
+            variant: BButtonVariant.outlined,
+            onPressed: () => context.pushNamed('signin'),
+          ),
+        ),
+      ],
+    );
+  }
+}
