d5681473a129b39a09eb0f09b0440fdc460f783b
Michael Elian Kevin <michaelkevin47@gmail.com>
2022-12-03 05:30:21 +0700

    chore(auth): update `AuthFailureMessages`

--- lib/constants/auth_failure_messages.dart
+++ lib/constants/auth_failure_messages.dart
@@ -4,7 +4,7 @@ class AuthFailureMessages {
   static const emailAlreadyInUse = 'Yahh, emailnya udah terdaftar.';
   static const weakPassword = 'Kata sandinya terlalu mudah.';
   static const invalidEmailOrPassword = 'Email atau kata sandi kamu salah.';
-  static const invalidEmail = 'Email yang kamu tulis ngga valid, nih.';
+  static const invalidEmail = 'Yahh, email yang kamu tulis ngga terdaftar.';
   static const timeout = 'Waktu habis. Coba cek koneksi internetmu ya.';
   static const unexpected = 'Duhh, ada kesalahan tak terduga, nih.';
 }
