8bdb3793b7b1798519c7b930850a458c5c261218
Michael Elian Kevin <michaelkevin47@gmail.com>
2022-12-09 20:52:43 +0700

    chore(booking): implement repositories and datasources

---
+++ lib/features/booking/domain/repositories/booking_repository.dart
@@ -0,0 +1,12 @@
+import 'package:fpdart/fpdart.dart';
+
+import '../../../../failures/failure.dart';
+import '../entities/appointment.dart';
+import '../entities/transaction_token.dart';
+
+abstract class BookingRepository {
+  Future<Either<Failure, TransactionToken>> getTransactionToken(
+    Appointment appointment,
+  );
+  Future<Either<Failure, Unit>> createAppointment(Appointment appointment);
+}
