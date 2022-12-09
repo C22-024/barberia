f58d14723f08a5103c6d67b2f65255c841684fad
Michael Elian Kevin <michaelkevin47@gmail.com>
2022-12-09 20:52:43 +0700

    chore(booking): implement repositories and datasources

---
+++ lib/features/booking/data/datasources/booking_remote_datasource.dart
@@ -0,0 +1,91 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:http/http.dart' as http;
+
+import '../../../../utils/firebase_extensions.dart';
+import '../models/appointment_model.dart';
+import '../models/transaction_token_model.dart';
+
+abstract class BookingRemoteDatasource {
+  Future<TransactionTokenModel> getTransactionToken(
+      AppointmentModel appointment);
+  Future<void> createAppointment(AppointmentModel appointment);
+}
+
+class BookingRemoteDatasourceImpl implements BookingRemoteDatasource {
+  const BookingRemoteDatasourceImpl(this._firestore);
+
+  final FirebaseFirestore _firestore;
+
+  int calculateGrossAmount(AppointmentModel appointment) {
+    int grossAmount = 0;
+    for (final element in appointment.paymentDetails.details) {
+      grossAmount += element.amount;
+    }
+    return grossAmount;
+  }
+
+  @override
+  Future<void> createAppointment(AppointmentModel appointment) async {
+    final pointBalanceDocRef =
+        _firestore.pointBalanceDocRef(appointment.user.id);
+    final appointmentDocRef = _firestore.appointmentColRef.doc();
+
+    await _firestore.runTransaction((transaction) async {
+      if (appointment.paymentDetails.channels.isNotEmpty) {
+        final snapshot = await transaction.get(pointBalanceDocRef);
+        final currentBalance = snapshot.get('currentBalance');
+        final payWithBPoinAmount =
+            appointment.paymentDetails.channels[0].amount;
+        if (currentBalance < payWithBPoinAmount) {
+          throw Exception('Insufficient BPoin balance.');
+        }
+        transaction.update(pointBalanceDocRef, {
+          'currentBalance': FieldValue.increment(-payWithBPoinAmount),
+        });
+      }
+      transaction.set(appointmentDocRef, appointment.toJson());
+    });
+  }
+
+  @override
+  Future<TransactionTokenModel> getTransactionToken(
+    AppointmentModel appointment,
+  ) async {
+    final url = Uri.https('app.sandbox.midtrans.com', 'snap/v1/transactions');
+    final headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic ${const String.fromEnvironment('AUTH_STRING')}',
+    };
+    final grossAmount = calculateGrossAmount(appointment);
+    final itemDetails = appointment.paymentDetails.details
+        .map((e) => {
+              'id': e.id,
+              'name': e.name,
+              'price': e.amount,
+              'quantity': 1,
+            })
+        .toList();
+    final body = {
+      'transaction_details': {
+        'order_id': appointment.id,
+        'gross_amount': grossAmount,
+      },
+      'customer_details': {
+        'first_name': appointment.user.name,
+      },
+      'item_details': itemDetails,
+    };
+
+    final response = await http.post(url, headers: headers, body: body);
+
+    if (response.statusCode != 201) {
+      throw Exception('Status Code: ${response.statusCode}');
+    }
+
+    final decoded = jsonDecode(response.body) as Map<String, dynamic>;
+    return TransactionTokenModel.fromJson(decoded);
+  }
+}
---
+++ lib/features/booking/data/repositories/booking_repository_impl.dart
@@ -0,0 +1,39 @@
+import 'package:fpdart/fpdart.dart';
+
+import '../../../../failures/failure.dart';
+import '../../domain/entities/appointment.dart';
+import '../../domain/entities/transaction_token.dart';
+import '../../domain/repositories/booking_repository.dart';
+import '../datasources/booking_remote_datasource.dart';
+import '../models/appointment_model.dart';
+
+class BookingRepositoryImpl implements BookingRepository {
+  const BookingRepositoryImpl(this._bookingRemoteDatasource);
+
+  final BookingRemoteDatasource _bookingRemoteDatasource;
+
+  @override
+  Future<Either<Failure, Unit>> createAppointment(
+      Appointment appointment) async {
+    try {
+      await _bookingRemoteDatasource
+          .createAppointment(AppointmentModel.fromDomain(appointment));
+      return right(unit);
+    } catch (e) {
+      return left(Failure.unexpected(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, TransactionToken>> getTransactionToken(
+    Appointment appointment,
+  ) async {
+    try {
+      final result = await _bookingRemoteDatasource
+          .getTransactionToken(AppointmentModel.fromDomain(appointment));
+      return right(result.toDomain());
+    } catch (e) {
+      return left(Failure.unexpected(e.toString()));
+    }
+  }
+}
