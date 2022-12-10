2194c871417070d9d70f0440cb6a3b477222d062
Teuku Zafran Elrifqi <57694121+zafran72@users.noreply.github.com>
2022-12-10 16:58:48 +0700

    chore(activities): add `detail activities`

--- lib/features/activities/presentation/pages/history_page.dart
+++ lib/features/activities/presentation/pages/history_page.dart
@@ -1,14 +1,14 @@
 import 'package:barberia_ui/barberia_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:timeago/timeago.dart' as timeago;
+import 'package:go_router/go_router.dart';
 
 import '../bloc/appoinment_getter/appoinment_getter_bloc.dart';
-import '../widgets/status_appointment_widget.dart';
+import '../widgets/activity_item_card_widget.dart';
 import 'activities_page.dart';
 
 class HistoryView extends StatelessWidget {
-  final List<String> appointmentHistory = <String>[
+  final List<String> filterStatusHistory = <String>[
     'expired',
     'rejected',
     'autoCanceled',
@@ -28,56 +28,30 @@ class HistoryView extends StatelessWidget {
             child: BText.body('Failed'),
           ),
           success: (appointments) {
-            if (appointments.isEmpty) {
-              return const EmptyView();
+            final appointmentsResult = appointments
+                .where((element) =>
+                    filterStatusHistory.contains(element.status['code']))
+                .toList();
+            if (appointmentsResult.isEmpty) {
+              return EmptyView(
+                iconData: Icons.history_rounded,
+                title: 'Tidak ada riwayat pemesanan',
+                subtitle:
+                    'Coba pesan layanan yang disediakan oleh barbershop yang ada.',
+                child: BButton(
+                  label: 'Mulai memesan',
+                  variant: BButtonVariant.primary,
+                  onPressed: () {
+                    context.goNamed('home');
+                  },
+                ),
+              );
             }
             return ListView.builder(
               padding: const EdgeInsets.all(8),
-              itemCount: appointments.length,
+              itemCount: appointmentsResult.length,
               itemBuilder: (BuildContext context, int index) {
-                final date = DateTime.fromMillisecondsSinceEpoch(
-                    appointments[index].status['updatedAt']);
-                if (appointmentHistory
-                    .contains(appointments[index].status['code'])) {
-                  return BCard(
-                    child: Container(
-                      padding: const EdgeInsets.all(10),
-                      margin: const EdgeInsets.only(right: 5),
-                      height: 100,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Container(
-                            margin: const EdgeInsets.only(right: 10),
-                            child: Image.asset('assets/images/activities.png'),
-                          ),
-                          Expanded(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                BText.titleSmall(
-                                  appointments[index].barbershop['name'],
-                                ),
-                                StatusAppointment(
-                                    status: appointments[index].status['code']),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    BText.caption(
-                                        timeago.format(date, locale: 'id')),
-                                  ],
-                                )
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                }
-                return Container();
+                return ActivityItemCard(appointment: appointmentsResult[index]);
               },
             );
           },
--- lib/features/activities/presentation/pages/process_page.dart
+++ lib/features/activities/presentation/pages/process_page.dart
@@ -1,21 +1,22 @@
 import 'package:barberia_ui/barberia_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
 import '../bloc/appoinment_getter/appoinment_getter_bloc.dart';
-import '../widgets/status_appointment_widget.dart';
+import '../widgets/activity_item_card_widget.dart';
 import 'activities_page.dart';
 
 class ProcessView extends StatelessWidget {
-  final List<String> appointmentOnProgress = <String>[
+  ProcessView({super.key});
+
+  final List<String> filterStatusOnProgress = <String>[
     'waitingForPayment',
     'paid',
     'accepted',
     'ongoing',
   ];
-
-  ProcessView({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -27,59 +28,32 @@ class ProcessView extends StatelessWidget {
             child: BText.body('Failed'),
           ),
           success: (appointments) {
-            if (appointments.isEmpty) {
-              return const EmptyView();
+            final appointmentsResult = appointments
+                .where((element) =>
+                    filterStatusOnProgress.contains(element.status['code']))
+                .toList();
+            if (appointmentsResult.isEmpty) {
+              return EmptyView(
+                iconData: Icons.history_edu_rounded,
+                title: 'Tidak ada pesanan berlangsung',
+                child: BButton(
+                  label: 'Mulai memesan',
+                  variant: BButtonVariant.primary,
+                  onPressed: () {
+                    context.goNamed('home');
+                  },
+                ),
+              );
+            } else {
+              return ListView.builder(
+                padding: const EdgeInsets.all(8),
+                itemCount: appointmentsResult.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ActivityItemCard(
+                      appointment: appointmentsResult[index]);
+                },
+              );
             }
-            return ListView.builder(
-              padding: const EdgeInsets.all(8),
-              itemCount: appointments.length,
-              itemBuilder: (BuildContext context, int index) {
-                final appointment = appointments[index];
-                final date = DateTime.fromMillisecondsSinceEpoch(
-                    appointment.status['updatedAt']);
-                if (appointmentOnProgress
-                    .contains(appointments[index].status['code'])) {
-                  return BCard(
-                    child: Container(
-                      padding: const EdgeInsets.all(10),
-                      margin: const EdgeInsets.only(right: 5),
-                      height: 100,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Container(
-                            margin: const EdgeInsets.only(right: 10),
-                            child: Image.asset('assets/images/activities.png'),
-                          ),
-                          Expanded(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                BText.titleSmall(
-                                  appointment.barbershop['name'],
-                                ),
-                                StatusAppointment(
-                                    status: appointment.status['code']),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    BText.caption(
-                                        timeago.format(date, locale: 'id')),
-                                  ],
-                                )
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                }
-                return Container();
-              },
-            );
           },
           orElse: () => const Center(
             child: CircularProgressIndicator(),
